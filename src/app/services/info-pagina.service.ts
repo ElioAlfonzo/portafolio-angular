bab4ee04a8ab1935db6e1b3de779e6dc254d9765
Elio Alfonzo <alfonzoelio7@gmail.com>
2019-04-27 23:43:18 -0400

    Fin de la parte 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,20 +9,34 @@ export class InfoPaginaService {
   
   info: InfoPagina = {}; //tipo any
   cargada = false; // tipo boolean
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    console.log('Servicio de InfoPagina Listo');
+    
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+
+  private cargarInfo(){
     //Leer el archivo Json
     //definimos donde esta la infor
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => { //con esta fncion de flecha nos traemos la infor de array
-        
         this.cargada= true;
         this.info = resp; //me trae la info como un array
-        console.log(resp);
+        // console.log(resp);
         
       });
+  }
 
-   }
+  private cargarEquipo(){
+    this.http.get('https://angular-web-2676a.firebaseio.com/equipo.json')
+      .subscribe( (resp:any[]) => { //con esta fncion de flecha nos traemos la infor de array
+        
+        this.equipo = resp; //me trae la info como un array
+        // console.log(resp);
+    });
+  }
+  
 }
